61d62a9a2393cc96364a1ef94de5b1242deb7f66
DimaHlushchenko <153325121+DimaHlushchenko@users.noreply.github.com>
2024-08-02 15:25:02 +0300

    save

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
-// Книга контактів
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './components/App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
